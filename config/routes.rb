85cc133e92d20b6d4ae6e0698f155274e2ba980c
MichaelOT93 <michaelotoole93@gmail.com>
2020-07-29 17:21:38 +0100

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+
+
+
+  get 'home' => 'static_pages#home'
+
+  get  '/about' => 'static_pages#about' 
+  get '/contact' => 'static_pages#contact'
+  get 'cart/index' => 'cart#index'
+
+  get '/cart/:id', to: 'cart#add'
+
+  root :to => 'static_pages#home'
+
+
+  devise_for :users
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
